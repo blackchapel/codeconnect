fe7de998f25eea0c769f2789d9c5617e98720dc2
Vidhita Pai <optimus@vd.local>
2022-04-09 17:44:30 +0530

    Post model created

---
+++ backend/models/post.js
@@ -0,0 +1,31 @@
+// Importing modules
+const mongoose = require('mongoose');
+const User = require('./user');
+
+// Creating the schema
+const postSchema = new mongoose.Schema(
+    {
+        caption: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        content: {
+            type: String,
+            required: true,
+        },
+
+        author: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: 'User'
+        }
+    },
+    { timestamps: true },
+    { versionKey: false }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+// Exporting modules
+module.exports = Blog;
