c434d4b51e61015f0b1e9d2e79dd88c7c09998c2
Prateek <prateekranka1607@gmail.com>
2022-04-09 15:15:51 +0530

    minor changes

---
+++ frontend/src/Components/Gmeet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+const Gmeet = () => {
+  return (
+    <>
+        <Button variant="contained" href="http://meet.google.com/new">Start a meet</Button>
+    </>
+  )
+}
+
+export default Gmeet
