f0412e181d1aa2943529509420457aebd2815261
Vidhita Pai <optimus@vd.local>
2022-04-09 20:56:37 +0530

    view posts on homepage

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,7 +5,8 @@ const passportSetup = require('../config/oauth');
 
 const {
     createPost,
-    viewUserPosts
+    viewUserPosts,
+    view10Posts
 } = require('../controllers/post');
 
 // Initializing router
@@ -13,6 +14,7 @@ const router = new express.Router();
 
 router.post('/create', passport.authenticate('github'), createPost);
 router.get('/all', viewUserPosts);
+router.get('/random', view10Posts);
 
 // Exporting Modules
 module.exports = router;
