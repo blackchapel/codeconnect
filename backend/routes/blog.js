a1ee43d0ee521639354c31fedfbcbd817855dfa2
Vidhita Pai <optimus@vd.local>
2022-04-09 19:53:42 +0530

    post and blog routers updated

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,7 +1,11 @@
 // Importing modules
 const express = require('express');
 const verifyjwt = require('../middleware/auth');
+const passport = require('passport');
+const passportSetup = require('../config/oauth');
+
 const {
+    createBlog,
     viewUserBlogs,
     viewBlog
 } = require('../controllers/blog');
@@ -9,6 +13,7 @@ const {
 // Initializing router
 const router = new express.Router();
 
+router.post('/create', passport.authenticate('github'), createBlog);
 router.get('/all', viewUserBlogs);
 router.get('/:id', viewBlog);
 
